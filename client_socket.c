1382efc6c5333dda75aa94c5ede45a5cb87d4ced
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-03-27 08:04:19 +0530

    adding the HTTP server

--- client_socket.c
+++ client_socket.c
@@ -11,7 +11,7 @@ int main()
     int network_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9302);
+    server_address.sin_port = htons(9002);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
     int connection_status = connect(network_socket, (struct sockaddr *)&server_address, sizeof(server_address));
