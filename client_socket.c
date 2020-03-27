3f85da7365bcc32425084ad10022aea24f5ab5d5
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-03-27 07:25:34 +0530

    adding the TCP protocol for basic communication

---
+++ client_socket.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+int main()
+{
+    int network_socket = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9302);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    int connection_status = connect(network_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+    if (connection_status == -1)
+        printf("something went wrong bitch! now fix that shit!");
+
+    char server_response[256];
+    recv(network_socket, &server_response, sizeof(server_response), 0);
+    printf("MSG FROM SERVER - %s/n", server_response);
+
+    close(network_socket);
+    return 0;
+}
